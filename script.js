7ae74f7463b3610d6e5e22b31cea41fa8b35f5d6
Barzi jaf <barzi4greatness@gmail.com>
2020-10-26 20:57:10 +0100

    some tiny changes

--- script.js
+++ script.js
@@ -6,14 +6,14 @@ let clickableSections = document.querySelectorAll('.content-switcher .section, .
 clickableSections.forEach(clickable => {
   clickable.addEventListener('click', function() {
     clickableSections.forEach(allsec => allsec.classList.remove('active'));
-    content_sections.forEach(sec => sec.classList.remove('display'));
+    content_sections.forEach(sec => sec.classList.remove('active'));
     var ID_of_clicked_Section = this.id;
     for(x = 0; x < clickableSections.length; x++)
     {
       if(clickableSections[x].id == ID_of_clicked_Section)
       {
         clickableSections[x].classList.add('active');
-        content_sections[x].classList.add('display');
+        content_sections[x].classList.add('active');
       }
     }
   })
