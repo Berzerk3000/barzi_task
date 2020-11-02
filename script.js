ac1864267120fbe7e49dac193f6aae8af3588abd
Barzi jaf <barzi4greatness@gmail.com>
2020-11-02 03:43:49 +0100

    Adding the overlay thing

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
-const content_sections = Array.from(document.querySelectorAll('.content section'));
-const clickable_sections = Array.from(document.querySelectorAll('.content-switcher .section, .links li a'));
-const allElements = content_sections.concat(clickable_sections);
+const contentSections = Array.from(document.querySelectorAll('.content section'));
+const clickableSections = Array.from(document.querySelectorAll('.content-switcher .section, .links li a'));
+const allElements = contentSections.concat(clickableSections);
 
-clickable_sections.forEach(clickable => {
+clickableSections.forEach(clickable => {
   clickable.addEventListener('click', function() {
-
-    var index_of_clicked_section = this.dataset.index;
     allElements.forEach(allElem => {
-      allElem.classList.remove('active');
-      if(allElem.dataset.index == index_of_clicked_section)
-      {
-        allElem.classList.add('active');
-      }
+      allElem.classList.remove('active'); 
+      if(allElem.dataset.index == this.dataset.index) allElem.classList.add('active');
     });
   })
 });
+
+
+
+const overlay = document.querySelectorAll('.overlay, .close-btn, .imp-text, .ipm');
+const closeBtn = overlay[0]; 
+const ipmBtn = overlay[3];
+
+ipmBtn.addEventListener('click', function() {
+  overlay.forEach(overlayEqupments => {
+    overlayEqupments.classList.add('active');
+  })
+})
+
+
+closeBtn.addEventListener('click', function () {
+  overlay.forEach(overlayEqupments => {
+    overlayEqupments.classList.remove('active');
+  })
+})
+
+
